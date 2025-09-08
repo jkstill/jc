91b3572ead6a8a62efe5cfa81c0a1a4d4466c6e9
Kelly Brazil <kellyjonbrazil@gmail.com>
2025-09-08 15:04:48 -0700

    parser version bump

--- jc/parsers/pacman.py
+++ jc/parsers/pacman.py
@@ -180,7 +180,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.0'
+    version = '1.1'
     description = '`pacman` command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
